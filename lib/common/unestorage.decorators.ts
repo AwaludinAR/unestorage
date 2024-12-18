0941fe3b4271acd0b9a21ade674f09e5c4188d54
Awaludin AR <me@awaludin.dev>
2024-12-18 17:42:14 +0700

    change InjectStorage options

--- lib/common/unestorage.decorators.ts
+++ lib/common/unestorage.decorators.ts
@@ -1,7 +1,13 @@
 import { Inject } from "@nestjs/common";
 import { getNamespaceToken, getStorageToken } from "./unestorage.utils";
+import { InjectStorageOptions } from "../interfaces";
 
-export function InjectStorage(namespace?: string, storageName?: string) {
+export function InjectStorage(opts?: string | InjectStorageOptions) {
+  if (!opts || typeof opts === "string") {
+    return Inject(getNamespaceToken(opts));
+  }
+
+  const { namespace, storageName } = opts;
   return Inject(getNamespaceToken(namespace, storageName));
 }
 
--- lib/interfaces/unestorage.interface.ts
+++ lib/interfaces/unestorage.interface.ts
@@ -2,3 +2,8 @@ export interface NamespaceDefinition {
   name?: string;
   base?: string;
 }
+
+export interface InjectStorageOptions {
+  namespace?: string;
+  storageName?: string;
+}
