5f98f846562b6c333f1e41c4a289180ae84c9524
Awaludin AR <me@awaludin.dev>
2025-10-02 07:24:41 +0700

    update release conf

--- release.config.cjs
+++ release.config.cjs
@@ -2,14 +2,14 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-  repositoryUrl: "https://github.com/AwaludinAR/unestorage.git",
-  branches: ["main", { name: "beta", channel: "beta", prerelease: true }],
+  repositoryUrl: 'https://github.com/AwaludinAR/unestorage.git',
+  branches: ['main', 'next'],
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/github",
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
     [
-      "@semantic-release/npm",
+      '@semantic-release/npm',
       {
         npmPublish: true,
       },
